b78b9018efb276ee0e1077aeb754134d9591880f
RadheshamWayal <shamwayal7@gmail.com>
2026-08-24 14:26:39 +0530

    Update Login Page

--- frontend/src/pages/Patient/PatientDashboard.jsx
+++ frontend/src/pages/Patient/PatientDashboard.jsx
@@ -184,7 +184,7 @@ function PatientDashboard() {
         setPrescriptions([]);
       })
       .finally(() => {
-        setLoading(false);
+        setPrescriptionsLoading(false);
       });
   }, [user]);
 
